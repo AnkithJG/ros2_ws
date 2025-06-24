f7e4248618db63b6eac9e757ac2f9fd1756d24b2
Ankith Ganji <ganjiankith@gmail.com>
2025-06-24 09:48:02 -0500

    fixing comments

--- src/lane_follower/lane_follower/lane_sensor.py
+++ src/lane_follower/lane_follower/lane_sensor.py
@@ -26,16 +26,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-# This code defines a ROS2 node that simulates a lane sensor by publishing a sinusoidal offset value.
-# The offset value simulates the drifting of the vehicle from the center of the lane.
-# The node publishes this value to the 'lane_offset' topic at a rate of 10 Hz.
-# The offset is calculated using a sine function, which varies over time to simulate the vehicle's movement.
-# The node can be run in a ROS2 environment, and it will continuously publish the offset value until it is shut down.
-# This can be useful for testing lane-following algorithms or for simulating sensor data in a controlled environment.
-# The offset value is published as a Float32 message, which is a standard message type in ROS2 for representing floating-point numbers.
-# The `publish_offset` method is called every 0.1 seconds, updating the offset based on the current time.
-# The `main` function initializes the ROS2 node and starts spinning it to process callbacks.
-# The node can be extended or modified to include more complex behavior or additional sensors as needed.
-# The `LaneSensor` class inherits from `Node`, which is the base class for all      ROS2 nodes.
-# The `create_publisher` method is used to create a publisher for the 'lane_offset' topic, allowing other nodes to subscribe to it and receive the lane offset data.
-# The `
